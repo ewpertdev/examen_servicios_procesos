f427e48dc69671be38dbc2e94e2fa72754b0b5d0
ewpertdev <xblackcat93@gmail.com>
2024-09-23 10:08:09 +0200

    Añadido constructor para clase Vehiculo

--- src/Ejercicio_1/Main.java
+++ src/Ejercicio_1/Main.java
@@ -4,5 +4,13 @@ class Vehiculo {
     private static int contador;
     String marca;
     String modelo;
+
+    public Vehiculo(String marca, String modelo, int año) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+    }
+
     int año;
+    String propietario;
 }
