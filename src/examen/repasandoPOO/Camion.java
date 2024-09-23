c4dbf7583146ae12e2ca8607603799b8957fad86
ewpertdev <xblackcat93@gmail.com>
2024-09-23 11:25:17 +0200

    fix

--- src/examen/repasandoPOO/Camion.java
+++ src/examen/repasandoPOO/Camion.java
@@ -1,6 +1,6 @@
 package examen.repasandoPOO;
 
-public class Camion extends Vehiculo{
+public class Camion extends Vehiculo {
 
     public Camion(String marca, String modelo, int ano, int capacidadCarga) {
         super(marca, modelo, ano);
--- src/examen/repasandoPOO/Main.java
+++ src/examen/repasandoPOO/Main.java
@@ -3,30 +3,25 @@ package examen.repasandoPOO;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Persona persona1 = new Persona("Ana");
         Persona persona2 = new Persona("Juan");
 
 //        Vehiculo[] veh = new Vehiculo[3];
-        Vehiculo coche1= new Coche("Toyota","Civic",2020,4);
+        Vehiculo coche1 = new Coche("Toyota", "Civic", 2020, 4);
         coche1.setPropietario("Juan");
 
-        Vehiculo coche2= new Coche("Honda","Civic", 2021,4 );
+        Vehiculo coche2 = new Coche("Honda", "Civic", 2021, 4);
         coche2.setPropietario("Ana");
-        Vehiculo coche3=new Coche("Ford","Focus",2022,4);
+
+        Vehiculo coche3 = new Coche("Ford", "Focus", 2022, 4);
         coche3.setPropietario(null);
 
-        Vehiculo camion1= new Camion("Mercedes","Actros",2019,18);
+        Vehiculo camion1 = new Camion("Mercedes", "Actros", 2019, 18);
         camion1.setPropietario("Juan");
 
         System.out.println("Total de vehículos creados: " + Vehiculo.getContador());
         ArrayList<Coche> Array
-
-
-
-
-
-
 
 
     }
